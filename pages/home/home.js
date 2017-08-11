8a3653b9e78dfa5fbcc3574f2ac459584b988ddb
lxllxllxs <774785161@qq.com>
2017-08-11 18:59:45 +0800

    1

--- pages/home/home.js
+++ pages/home/home.js
@@ -24,5 +24,24 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
-  }
+  },
+ jumpTo: function(event) {
+   //从0开始
+    var menu=event.currentTarget.dataset.menu
+    console.log(menu)
+    switch (menu){
+      case 1:
+      break;
+       case 2:
+      break;
+       case 3:
+      break;
+           case 4:
+      break;
+    }
+    wx.navigateTo({
+    url: '../TipDetail/TipDetail?tipId='+tipOffId+'&title='+title
+  })
+  },
+
 })
